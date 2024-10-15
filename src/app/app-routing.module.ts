b7853e0f4cc0aee312d1a48736fc3a043617c257
Rahmansgit <151901014+netjetit@users.noreply.github.com>
2024-10-15 18:02:30 +0800

    angular project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'footer', component: FooterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
